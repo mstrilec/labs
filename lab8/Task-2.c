bbfcb80917acc6f898976d66ef2489e4e5b4fc78
m.strilec <59706893+mstrilec@users.noreply.github.com>
2024-06-11 19:48:34 +0300

    Update Task-2.c

--- lab8/Task-2.c
+++ lab8/Task-2.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-void inputArray(int *array, int n) {
+void inputArray(int array[], int n) {
     srand(time(NULL));
     printf("Масив: ");
     for (int i = 0; i < n; i++) {
-        array[i] = rand() % 201 - 100;
+        array[i] = rand() % 21 - 10;
         printf("%d ", array[i]);
     }
     printf("\n");
 }
 
-int findMinAbsIndex(int *array, int n) {
+int findMinAbsIndex(int array[], int n) {
     int minAbsIndex = 0;
     for (int i = 1; i < n; i++) {
         if (abs(array[i]) < abs(array[minAbsIndex])) {
@@ -22,7 +23,7 @@ int findMinAbsIndex(int *array, int n) {
     return minAbsIndex;
 }
 
-int productBetweenZeros(int *array, int n) {
+int productBetweenZeros(int array[], int n, bool *foundZeros) {
     int firstZeroIndex = -1;
     int secondZeroIndex = -1;
     int product = 1;
@@ -39,9 +40,12 @@ int productBetweenZeros(int *array, int n) {
     }
 
     if (firstZeroIndex != -1 && secondZeroIndex != -1) {
+        *foundZeros = true;
         for (int i = firstZeroIndex + 1; i < secondZeroIndex; i++) {
             product *= array[i];
         }
+    } else {
+        *foundZeros = false;
     }
 
     return product;
@@ -65,8 +69,13 @@ int main() {
         int minAbsIndex = findMinAbsIndex(array, n);
         printf("Номер мінімального за модулем елемента масиву: %d\n", minAbsIndex);
 
-        int product = productBetweenZeros(array, n);
-        printf("Добуток елементів масиву, розташованих між першим й другим нульовими елементами: %d\n", product);
+        bool foundZeros;
+        int product = productBetweenZeros(array, n, &foundZeros);
+        if (foundZeros) {
+            printf("Добуток елементів масиву, розташованих між першим й другим нульовими елементами: %d\n", product);
+        } else {
+            printf("В масиві недостатньо нульових елементів для обчислення добутку.\n");
+        }
 
         printf("Натисніть 1, щоб повторити програму або 0, щоб вийти: ");
         if (!(scanf("%d", &repeat)) || (repeat != 0 && repeat != 1)) {
